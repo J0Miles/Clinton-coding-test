94c02fddb839be766433d8685a08477ad1b4ded4
NeutralDread <jordanamiles@hotmail.com>
2019-06-18 21:19:34 -0500

    imported react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Form from './Form.js';
+import Form from './components/Form.js';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>React Form Validation Demo</h2>
+          <h2>User Account Manager</h2>
         </div>
         <Form />
       </div>
